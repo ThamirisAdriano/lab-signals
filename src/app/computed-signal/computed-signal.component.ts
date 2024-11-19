2a6a3f4f47611368e89d26ec2934aa32be90ccb8
thamiris <tsadriano.band.com.br>
2024-11-19 07:11:17 -0300

    aula 5

---
+++ src/app/computed-signal/computed-signal.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Elemento, ElementoService } from '../elemento.service';
+
+@Component({
+  selector: 'app-computed-signal',
+  templateUrl: './computed-signal.component.html',
+  styleUrls: ['./computed-signal.component.css']
+})
+export class ComputedSignalComponent {
+
+  elementos: Elemento[];
+
+  constructor(public elementoService: ElementoService) {
+    this.elementos = this.elementoService.elementos;
+  }
+  
+  selecionarElemento1(elemento: Elemento) {
+    this.elementoService.selecionarElemento1(elemento);
+  }
+
+  selecionarElemento2(elemento: Elemento) {
+    this.elementoService.selecionarElemento2(elemento);
+  }
+
+  elementoSelecionado1() {
+    return this.elementoService.elementoSelecionado1();
+  }
+
+  elementoSelecionado2() {
+    return this.elementoService.elementoSelecionado2();
+  }
+
+  massaAtomicaTotal() {
+    return this.elementoService.massaAtomicaTotal();
+  }
+
+}
--- src/app/elemento.service.ts
+++ src/app/elemento.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal, effect, computed } from '@angular/core';
+import { Injectable, signal, effect, computed, untracked } from '@angular/core';
 
 export interface Elemento {
   nome: string;
@@ -6,6 +6,8 @@ export interface Elemento {
   numeroMassa: number;
   pontoFusao: number; 
   pontoEbulicao: number;
+  numeroNeutrons: number;
+  numeroAtomico: number;
 }
 
 @Injectable({
@@ -13,19 +15,22 @@ export interface Elemento {
 })
 export class ElementoService {
   elementoSelecionado = signal<Elemento | null>(null);
+  elementoSelecionado1 = signal<Elemento | null>(null);
+  elementoSelecionado2 = signal<Elemento | null>(null);
   temperatura = signal<number>(25);
   estadoFisico = signal<string>('');
   private favoritos = signal<Elemento[]>([]);
 
 
   elementos: Elemento[] = [
-    { nome: 'Hidrogênio', simbolo: 'H', numeroMassa: 1, pontoFusao: -259, pontoEbulicao: -253 },
-    { nome: 'Carbono', simbolo: 'C', numeroMassa: 12, pontoFusao: 3550, pontoEbulicao: 4027 },
-    { nome: 'Nitrogênio', simbolo: 'N', numeroMassa: 14, pontoFusao: -210, pontoEbulicao: -196 },
-    { nome: 'Oxigênio', simbolo: 'O', numeroMassa: 16, pontoFusao: -218, pontoEbulicao: -183 },
-    { nome: 'Sódio', simbolo: 'Na', numeroMassa: 23, pontoFusao: 98, pontoEbulicao: 883 },
-    { nome: 'Cloro', simbolo: 'Cl', numeroMassa: 35, pontoFusao: -101, pontoEbulicao: -34 }
+    { nome: 'Hidrogênio', simbolo: 'H', numeroAtomico: 1, numeroNeutrons: 0, numeroMassa: 1, pontoFusao: -259, pontoEbulicao: -253 },
+    { nome: 'Carbono', simbolo: 'C', numeroAtomico: 6, numeroNeutrons: 6, numeroMassa: 12, pontoFusao: 3550, pontoEbulicao: 4027 },
+    { nome: 'Nitrogênio', simbolo: 'N', numeroAtomico: 7, numeroNeutrons: 7, numeroMassa: 14, pontoFusao: -210, pontoEbulicao: -196 },
+    { nome: 'Oxigênio', simbolo: 'O', numeroAtomico: 8, numeroNeutrons: 8, numeroMassa: 16, pontoFusao: -218, pontoEbulicao: -183 },
+    { nome: 'Sódio', simbolo: 'Na', numeroAtomico: 11, numeroNeutrons: 12, numeroMassa: 23, pontoFusao: 98, pontoEbulicao: 883 },
+    { nome: 'Cloro', simbolo: 'Cl', numeroAtomico: 17, numeroNeutrons: 18, numeroMassa: 35, pontoFusao: -101, pontoEbulicao: -34 }
   ];
+
 
   constructor() {
     effect(() => {
@@ -71,4 +76,24 @@ export class ElementoService {
       fav.filter((e) => e !== elemento)
     );
   }
+
+  massaAtomicaTotal = computed(() => {
+    const elemento1 = this.elementoSelecionado1();
+    const elemento2 = this.elementoSelecionado2();
+    const massa1 = untracked(() => elemento1 ? elemento1.numeroAtomico + elemento1.numeroNeutrons : 0);
+    const massa2 = untracked(() => elemento2 ? elemento2.numeroAtomico + elemento2.numeroNeutrons : 0);
+ 
+    return massa1 + massa2;
+  });
+
+  selecionarElemento1(elemento: Elemento) {
+    this.elementoSelecionado1.set(elemento);
+  }
+ 
+ 
+  selecionarElemento2(elemento: Elemento) {
+    this.elementoSelecionado2.set(elemento);
+  }
+ 
+ 
 }
